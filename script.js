607c6080c3fcbb840731c835c52bd2a534fd7bde
Mbonyumugisha-Prince <p.mbonyumug@alustudent.com>
2025-02-24 14:23:42 +0200

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["", "Fullstack engineer", "devOps dev", "Mobile dev"], // Corrected property name
+    typeSpeed: 100,
+    backSpeed: 60, // Corrected property name
+    loop: true
+});
